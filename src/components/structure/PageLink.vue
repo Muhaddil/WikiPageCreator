690381b0da2fc86c70953d878a28bf679e1f1999
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-02-13 02:19:48 +0100

    Update 0.8.50 - Quality of Life Improvements and Visual Improvements

--- src/components/structure/PageLink.vue
+++ src/components/structure/PageLink.vue
@@ -1,3 +1,12 @@
+<template>
+  <a
+    :href="url"
+    :title="title"
+    class="button"
+    >{{ text }}</a
+  >
+</template>
+
 <script setup lang="ts">
 import type { PageLinkProps } from '@/types/objects';
 import { computed } from 'vue';
@@ -11,17 +20,46 @@ const title = computed(() => {
 });
 </script>
 
-<template>
-  <a
-    :href="url"
-    :title="title"
-    class="button is-link is-outlined"
-    >{{ text }}</a
-  >
-</template>
-
 <style scoped lang="scss">
-a {
-  font-size: 1.5rem;
+a.button {
+  background-color: var(--boton-backgoundcolor);
+  color: var(--boton-fontcolor);
+  font-weight: bold;
+  padding: 15px 30px;
+  text-decoration: none;
+  border-radius: 5px;
+  box-shadow: 2px 2px 4px rgba(167, 166, 177, 0.5);
+  transition: all 0.5s ease;
+  margin-bottom: -15px;
+  display: flex;
+  font-size: 1.2em;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: rgb(197, 0, 1);
+    transform: scale(1.2);
+  }
 }
 </style>
+
+<!-- a.button {
+  background-color: var(--boton-backgoundcolor);
+  color: var(--boton-fontcolor);
+  font-weight: bold;
+  padding: 15px 30px;
+  text-decoration: none;
+  border-radius: 5px;
+  box-shadow: 2px 2px 4px rgba(167, 166, 177, 0.5);
+  transition: all 0.5s ease;
+  margin-bottom: -10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.2em;
+
+  &:hover {
+    background-color: rgb(197, 0, 1);
+    transform: scale(1.2);
+  }
+} -->
