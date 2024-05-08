01af07548a7e1162b5a3ec59167fc777556d3061
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-05-08 17:41:17 +0000

    Deploying to gh-pages from @ Muhaddil/RSSWikiPageCreator@caf0bacc521cd358a8390452c0bb498550c1a18d 🚀

---
+++ assets/dateLogic-MoINhAtz.js
@@ -0,0 +1,1 @@
+import{w as a}from"./links-Cs2zMmC8.js";function s(t){const s=t.value,n=t.dataset.destNoauto;a(s.replaceAll("-","/"),n)}function n(t){const s=(new Date).getFullYear();a(s.toString(),t)}export{n as g,s as p};
