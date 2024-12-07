b203ffd927edfd547c59bbcdb51f9a70269f86ba
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-12-07 14:56:27 +0100

    Remove unused pages

--- src/variables/mineral/nutsource.ts
+++
@@ -1,7 +0,0 @@
-import type { SelectOption } from '@/types/selectInputOptions';
-import floranutSourceDatalist from '@/datalists/floraDatalists3';
-
-export const mappedFloraNutSource: SelectOption[] = Object.entries(floranutSourceDatalist).map(([english, spanish]) => ({
-  label: spanish,
-  value: english
-}));
--- src/variables/mineral/roots.ts
+++
@@ -1,7 +0,0 @@
-import type { SelectOption } from '@/types/selectInputOptions';
-import florarootDatalist from '@/datalists/floraDatalists5';
-
-export const mappedFloraRoots: SelectOption[] = Object.entries(florarootDatalist).map(([english, spanish]) => ({
-  label: spanish,
-  value: english
-}));
